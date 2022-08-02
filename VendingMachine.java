c0df2256eb7ca1abe3f2a6ff64c39c9ac133e7b3
ctshara14 <ctsharath14@gmail.com>
2022-08-02 12:20:34 +0530

    Adding: Vending Machine Program

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,30 +1,44 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class VendingMachine {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value for which you want to get the change");
+        int n = sc.nextInt();
+        int a[] = {1, 2, 5, 10, 50, 100, 500, 1000};
 
-            int arr[] = {1000,500,100,50,10,5,2,1};
-            int money, totalNotes=0, count=0;
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
 
-            Scanner r = new Scanner(System.in);
-            System.out.println("Enter Money: ");
-            money = r.nextInt();
+        int ans = minCoins(n, a, dp);
+        System.out.println("The minimum number of coins required will be : "+ans);
 
-            int copy=money;
-
-            for(int i=0;i<8;i++)
-            {
-                // counting number of notes.
-                count=money/arr[i];
-                if(count!=0) {
-                    System.out.println(arr[i]+"   " +count+ "  "+arr[i]*count);
-                }
-                totalNotes=totalNotes+count;
-                money=money%arr[i];
-            }
-            System.out.println("TOTAL= " +copy);
-            System.out.println("Minimum Number of Notes required= " +totalNotes);
-            r.close();
-        }
-
+        /*for(int x: dp) {
+            System.out.print(x+" ");
+        }*/
     }
+
+    static int minCoins(int n, int a[], int dp[]) {
+
+        if(n == 0) return 0;
+
+        int ans = Integer.MAX_VALUE;
+
+        for(int i = 0; i<a.length; i++) {
+            if(n-a[i] >= 0) {
+                int subAns = 0;
+                if(dp[n-a[i]] != -1) {
+                    subAns = dp[n-a[i]];
+                } else {
+                    subAns = minCoins(n-a[i], a, dp);
+                }
+                if(subAns != Integer.MAX_VALUE &&
+                        subAns + 1 < ans) {
+                    ans = subAns + 1;
+                }
+            }
+        }
+        return dp[n] = ans;
+    }
+}
